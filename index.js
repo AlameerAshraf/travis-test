fd0884772961a6f4985f4f6582d0ab1cdc19e582
Alameer Ashraf <alameer.ashraf@schneider-electric.com>
2019-10-10 13:50:32 +0200

    test

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var express = require('express');
 
 var app = express();
 
-app.get('/Home' , (req , res) => {
+app.get('/Home' , function (req , res){
     res.send("Hi this is home !");
     res.end();
 });
 
-app.listen(4875 ,() => {
+app.listen(4875 ,function () {
     console.log("App is running on port !");
 });
